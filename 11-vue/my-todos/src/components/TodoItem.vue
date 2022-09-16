24d83faa53498d98cac5ccd69257239a12d1de20
Nicholas <Nicholas@example.com>
2022-09-16 16:27:45 +0800

    todo

---
+++ 11-vue/my-todos/src/components/TodoItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <input
+      type="checkbox"
+      :checked="todo.finish"
+      @change="
+        $emit('toggle-todo-list-item', {
+          id: todo.id,
+          finish: $event.target.checked,
+        })
+      "
+      
+    />
+    <!-- $event.target.checked  $event 是在原生事件里面 指向DOM事件对象 -->
+    <!-- <input type="checkbox" v-model="todo.finish" /> -->
+    <span>{{ todo.text }}</span>
+    <i> X</i>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: Object,
+  },
+};
+</script>
+
+<style lang="less" >
+</style>
